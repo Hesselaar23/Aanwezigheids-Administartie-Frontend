02e5bb0bfef2860151faabd563903b584729589f
Hessel van Bakel <Hesselvanbakel@gmail.com>
2024-06-25 14:46:01 +0200

    Final base version server
    I hope this works

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -4,25 +4,12 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import useMedewerkers from "../api/useMedewerkers";
 import { Medewerker } from "@/lib/types";
 import config from '@/lib/ip_config.json';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
-
 
 
 export default function Overview() {
   const { medewerkers, loading, error, updateMedewerker } = useMedewerkers();
   const ipAddress = config.ipAddress;
   const [cardColors, setCardColors] = useState<{ [key: string]: string }>({});
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = async (medewerker: Medewerker) => {
     let newStatus: 'aanwezig' | 'afwezig' | 'extern';
@@ -84,7 +71,7 @@ export default function Overview() {
   const RenderMedewerker = (({medewerker} : {medewerker : Medewerker}) => {
 
     const color = cardColors[medewerker.id] || '#ffffff'; 
-    const textColor = medewerker.attributes.aanwezigheid === 'afwezig' ? '#000000' : '#ffffff'; 
+    const textColor = medewerker.attributes.aanwezigheid === 'afwezig' ? '#000000' : '#ffffff';
   
   return (
     <Card key={medewerker.id} className="flex-auto m-2 select-none" style={{ backgroundColor: color }} onClick={() => {
@@ -106,11 +93,11 @@ export default function Overview() {
     <div className="font-bold h-screen flex flex-col items-center select-none">
       <div className="w-full flex justify-between items-center px-4 py-2">
         <div className="flex items-center">
-          <img src={`http://${ipAddress}:1337/uploads/kega_logo_b6a3f0a522.png`} className="h-20 w-auto" onClick={() => {document.exitFullscreen();}}/>
-          <img src={`http://${ipAddress}:1337/uploads/keephub_logo_cf719ef978.png`} className="h-20 w-auto" />
+          <img src={`https://original-peace-bc5b1fa571.media.strapiapp.com/small_kega_logo_b6a3f0a522_4999cbacc2.png`} className="h-20 w-auto" onClick={() => {document.exitFullscreen();}}/>
+          <img src={`https://original-peace-bc5b1fa571.media.strapiapp.com/keephub_logo_cf719ef978_e7e5955fda.png`} className="h-20 w-auto" />
         </div>
         <div className="flex items-center">
-          <h1 className="text-6xl" onClick={() => setIsOpen(true)}>Aanwezigheid</h1>
+          <h1 className="text-6xl">Aanwezigheid</h1>
         </div>
         <div className="flex items-center"> 
           <h1 className="mr-4 text-2xl">Aantal <br/> aanwezigen</h1>
